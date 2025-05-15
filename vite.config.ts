95dd04aaf1e4b5d4496ac1a1f6500072bebe2aa1
kevinmilespogi05 <kevinmilesjulhusin05@gmail.com>
2025-05-16 01:02:39 +0800

    oh no

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BASE_VITE_PATH || "/",
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path
-      },
-    },
-    open: '/login',
-  },
+  base: "/",
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
     chunkSizeWarningLimit: 1600
   },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
