077fe8c9143708848ee73d79f38ec24255a7ba28
kevinmilespogi05 <kevinmilesjulhusin05@gmail.com>
2025-05-16 13:36:03 +0800

    Reapply "Update axios.ts"
    This reverts commit a392ea46b324801f2d352c70e6193721eeaeb5ee.

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-// Create axios instance
-const axiosInstance = axios.create();
+// Create axios instance with base URL from environment variable
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+});
 
 // Add request interceptor
 axiosInstance.interceptors.request.use(
